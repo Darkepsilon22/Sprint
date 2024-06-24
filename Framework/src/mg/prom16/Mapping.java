d9b0249262ab8405dc3eee811d00339588fdfd5c
Darkepsilon22 <153285537+Darkepsilon22@users.noreply.github.com>
2024-06-24 20:01:10 +0300

    sprint7-2494

--- Framework/src/mg/prom16/Mapping.java
+++ Framework/src/mg/prom16/Mapping.java
@@ -1,28 +1,54 @@
 package mg.prom16;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+// import com.thoughtworks.paranamer.AdaptiveParanamer;
+// import com.thoughtworks.paranamer.Paranamer;
+
 public class Mapping {
     private String className;
-    private String methodName;
+    private Method method;
 
-    public Mapping(String className, String methodName) {
+    public Mapping(String className, Method method) {
         this.className = className;
-        this.methodName = methodName;
+        this.method = method;
     }
 
     public String getClassName() {
         return className;
     }
 
-    public String getMethodName() {
-        return methodName;
+    public Method getMethod() {
+        return method;
     }
 
     @Override
     public String toString() {
         return "Mapping{" +
                 "className='" + className + '\'' +
-                ", methodName='" + methodName + '\'' +
+                ", methodName='" + method.getName() + '\'' +
                 '}';
+    }
+
+    public String method_to_string() {
+        StringBuilder methodString = new StringBuilder();
+        methodString.append(method.getName()).append("(");
+
+        Parameter[] parameters = method.getParameters();
+
+        // Paranamer paranamer = new AdaptiveParanamer();
+        // String[] parameterMethodNames = paranamer.lookupParameterNames(method);
+        
+        for (int i = 0; i < parameters.length; i++) {
+            // methodString.append(parameters[i].getType().getSimpleName()+" "+parameterMethodNames[i]);
+            methodString.append(parameters[i].getType().getSimpleName()+" "+parameters[i].getName());
+            if (i < parameters.length - 1) {
+                methodString.append(", ");
+            }
+        }
+
+        methodString.append(")");
+        return methodString.toString();
     }
 }
 
