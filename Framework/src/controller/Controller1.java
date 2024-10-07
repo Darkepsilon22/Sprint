64ac69ecc202d52e8715a6cf3f212a87cfb86b9d
Darkepsilon22 <yohanrak61@gmail.com>
2024-10-07 19:14:39 +0300

    sprint11-2494

--- Framework/src/controller/Controller1.java
+++ Framework/src/controller/Controller1.java
@@ -5,56 +5,57 @@ import java.util.Vector;
 import Annotations.*;
 import mg.prom16.ModelView;
 import models.Employe;
+import java.util.List;
 
 @Controller
 public class Controller1 {
 
-    @Get(value = "/message")
-    public String get_message(String message) {
-        System.out.println("Message reçu : " + message); // Log
-        return message;
-    }
+    // @Post(value = "/message")
+    // public String get_message(String message) {
+    //     System.out.println("Message reçu : " + message); // Log
+    //     return message;
+    // }
 
-    @Post(value = "/saveEmploye")
-    public ModelView saveEmploye(@RequestBody Employe employe) {
-        System.out.println("Tentative d'enregistrement de l'employé : " + employe); // Log
-        ModelView mv = new ModelView();
-        if (employe.getNom() == null || employe.getPrenom() == null) {
-            mv.setUrl("/views/ErrorPage.jsp");
-            mv.addObject("message", "Erreur : Les informations de l'employé sont incomplètes.");
-            return mv;
-        }
-        mv.setUrl("/views/Employe.jsp");
-        mv.addObject("employe", employe);
-        return mv;
-    }
+    // @Post(value = "/saveEmploye")
+    // public ModelView saveEmploye(@RequestBody Employe employe) {
+    //     System.out.println("Tentative d'enregistrement de l'employé : " + employe); // Log
+    //     ModelView mv = new ModelView();
+    //     if (employe.getNom() == null || employe.getPrenom() == null) {
+    //         mv.setUrl("/views/ErrorPage.jsp");
+    //         mv.addObject("message", "Erreur : Les informations de l'employé sont incomplètes.");
+    //         return mv;
+    //     }
+    //     mv.setUrl("/views/Employe.jsp");
+    //     mv.addObject("employe", employe);
+    //     return mv;
+    // }
 
-    @Get(value = "/pageNotFound")
-    public ModelView pageNotFound() {
-        ModelView modelView = new ModelView();
-        modelView.setUrl("/views/ErrorPage.jsp");
-        modelView.addObject("message", "Page Not Found");
-        modelView.addObject("code", 404);
-        return modelView;
-    }
+    // @Get(value = "/pageNotFound")
+    // public ModelView pageNotFound() {
+    //     ModelView modelView = new ModelView();
+    //     modelView.setUrl("/views/ErrorPage.jsp");
+    //     modelView.addObject("message", "Page Not Found");
+    //     modelView.addObject("code", 404);
+    //     return modelView;
+    // }
 
-    @Get(value = "/date")
-    public java.util.Date get_Date() {
-        return new java.util.Date();
-    }
+    // @Get(value = "/date")
+    // public java.util.Date get_Date() {
+    //     return new java.util.Date();
+    // }
 
-    @Get(value = "/employe")
-    public ModelView get_employe(@Param(name = "id") String id, @Param(name = "nom") String nom, @Param(name = "prenom") String prenom) {
-        ModelView mv = new ModelView();
-        mv.setUrl("/views/Employe.jsp");
-        mv.addObject("id", id);
-        mv.addObject("nom", nom);
-        mv.addObject("prenom", prenom);
-        return mv;
-    }
+    // @Get(value = "/employe")
+    // public ModelView get_employe(@Param(name = "id") String id, @Param(name = "nom") String nom, @Param(name = "prenom") String prenom) {
+    //     ModelView mv = new ModelView();
+    //     mv.setUrl("/views/Employe.jsp");
+    //     mv.addObject("id", id);
+    //     mv.addObject("nom", nom);
+    //     mv.addObject("prenom", prenom);
+    //     return mv;
+    // }
 
     @Restapi
-    @Get(value = "/listemploye")
+    @Get(value = "/liste")
     public ModelView list_employe() {
         ModelView mv = new ModelView();
 
@@ -65,8 +66,8 @@ public class Controller1 {
 
         Employe employe2 = new Employe();
         employe2.setId("2");
-        employe2.setNom("Razafimanantsoa");
-        employe2.setPrenom("Coco");
+        employe2.setNom("Rakotoarison");
+        employe2.setPrenom("Yohan");
 
         Vector<Employe> listEmploye = new Vector<>();
         listEmploye.add(employe1);
@@ -88,4 +89,24 @@ public class Controller1 {
         System.out.println("Employé Yohan retourné : " + employe2); // Log
         return employe2;
     }
+    
+    @Get(value = "/get")
+    public ModelView testVerb() {
+        ModelView mv = new ModelView();
+        mv.setUrl("/views/TestVerb.jsp");
+        mv.addObject("message", "Cette méthode est appelée avec la méthode GET.");
+        return mv;
+    }
+
+    @Post(value = "/testVerbPost")
+    public ModelView testVerbPost() {
+        ModelView mv = new ModelView();
+        mv.setUrl("/views/TestVerb.jsp");
+        mv.addObject("message", "Cette méthode est appelée avec la méthode POST.");
+        return mv;
+    }
+
+    
+
+  
 }
